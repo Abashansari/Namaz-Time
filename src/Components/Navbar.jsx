0c6ce3187a188e3b64c84852fa3d9fc8eb2f5bde
Abash Ansari <ansariabash2004@gmail.com>
2025-11-12 23:00:20 +0530

    created a navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
+      <div className="container-fluid">
+
+        <a className="navbar-brand fw-bold text-primary d-flex align-items-center" href="#">
+          Namaz
+        </a>
+
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0 fw-semibold">
+            <li className="nav-item"><Link className="nav-link text-dark" to="/">Home</Link></li>
+            <li className="nav-item"><Link className="nav-link text-dark" to="/about">About</Link></li>
+            <li className="nav-item"><Link className="nav-link text-dark" to="/aboutNamaz">Namaz</Link></li>
+            <li className="nav-item"><a className="nav-link text-dark" href="#contact">Contact</a></li>
+            <li className="nav-item"><Link className="nav-link text-dark" aria-current="page" to="/register">Register</Link></li>
+            <li className="nav-item"><Link className="nav-link text-dark" aria-current="page" to="/login">Login</Link></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}

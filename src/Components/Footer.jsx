47f2a82abf8dcc145ec417885261a7a5f9a14d7e
Abash Ansari <ansariabash2004@gmail.com>
2025-11-23 19:28:41 +0530

    added new date() function

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="bg-light text-center py-3 shadow-sm mt-5">
       <p className="small mb-0 text-muted">
-        &copy; 2025 Designed by
+        &copy; {new Date().getFullYear()} Designed & Developed by
         <span className="fw-bold text-dark"> Abash Ansari The Great</span>. All Rights Reserved.
       </p>
     </footer>

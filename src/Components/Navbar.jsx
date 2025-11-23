63d9d7b5e9348f0af3c2babcd26ea72c2addff0b
Abash Ansari <ansariabash2004@gmail.com>
2025-11-23 19:29:17 +0530

    replace anchor tag with {Link} of react-router-dom

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -22,7 +22,7 @@ export default function Navbar() {
                         <li className="nav-item"><Link className="nav-link text-dark" to="/aboutNamaz">Namaz</Link></li>
                         <li className="nav-item"><Link className="nav-link text-dark" to="/register">Register</Link></li>
                         <li className="nav-item"><Link className="nav-link text-dark" to="/login">Login</Link></li>
-                        <li className="nav-item"><a className="nav-link text-dark" href="#contact">Contact</a></li>
+                        <li className="nav-item"><Link className="nav-link text-dark" to="/contact">Contact</Link></li>
                     </ul>
                 </div>
             </div>
